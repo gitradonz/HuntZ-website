d2669cdbc86eeb0bd43933a3b5ceb882f2628ab2
gitradonz <63265564+gitradonz@users.noreply.github.com>
2022-05-16 21:11:12 +0200

    Added images to homepage

--- components/header.js
+++ components/header.js
@@ -1,19 +1,13 @@
 import Link from 'next/link'
 import styles from '../styles/Header.module.css'
-import styled from 'styled-components'
 
-const StyledLink = styled(Link)`
-  color: red;
-  font-size: 20px;
-`
 export default function Header({ style }) {
   return (
     <nav className={style ? style : styles.header}>
-      <StyledLink href="/">Home</StyledLink>
-      <Link href="/projects">Projects</Link>
+      <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/contact">Contact</Link>
-      <Link href="/huntz">HuntZ</Link>
+      <Link href="/ranking">Ranking</Link>
     </nav>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,31 @@ import Image from 'next/image'
 import Footer from '../components/footer'
 import Header from '../components/header'
 import styles from '../styles/Home.module.css'
+import styled from 'styled-components'
+
+const StyledTitle = styled.h3`
+  font-size: 1.5rem;
+  text-align: center;
+  justify-content: center;
+`
+
+const StyledContent = styled.span`
+  font-size: 1rem;
+  text-align: end;
+`
+
+const StyledAnchor = styled.a`
+  color: #0070f3;
+  text-decoration: none;
+  font-size: 1rem;
+  `
+
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Pablo Ríos - Home</title>
+        <title>HUNTZ</title>
         <meta
           name="description"
           content="Pablo Rios website, generated with next"
@@ -19,9 +38,32 @@ export default function Home() {
       <div className={styles.container}>
         <Header style={styles.header} />
         <h1 className={styles.title}>
-          Welcome to <span className={styles.span}>test web</span>
+          {/* Welcome to <span className={styles.span}>HuntZ</span> */}
+          <Image src="/huntzLogo.png" alt="huntzLogo" width={800} height={400} />
         </h1>
-        <main className={styles.main}>CONTENIDO EN EL MAIN</main>
+        <div className={styles.main}>
+         <StyledTitle>
+          GAME DOWNLOAD IS <strong>AVAILABLE</strong> !! 
+          </StyledTitle>
+        </div>
+        <div className={styles.main}>
+        <Image src="/huntzMenu.png" alt="huntzMenuImage" width={877} height={500}/> 
+        <Image src="/huntz.png" alt="huntzImage" width={877} height={500}/>
+        </div>
+        <div className={styles.main}>
+
+        <StyledContent>
+            Contact with me <StyledAnchor href="mailto:pabloriosrdn@gmail.com">via mail</StyledAnchor> if you want to be a tester!
+          </StyledContent>
+          </div>
+
+        {/* <div className={styles.main}>
+          
+          
+          
+        
+        </div> */}
+        
 
         <Footer>
           <a
