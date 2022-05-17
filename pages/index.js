4e431c9f3a92f5a1b6203abce009c95e0085ec39
Pablo Rios <pablo.rios@lobocom.es>
2022-05-17 10:23:28 +0200

    Added footer images & style

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,26 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Footer from '../components/footer'
-import Header from '../components/header'
-import styles from '../styles/Home.module.css'
-import styled from 'styled-components'
+import Head from "next/head";
+import Image from "next/image";
+import Footer from "../components/footer";
+import Header from "../components/header";
+import styles from "../styles/Home.module.css";
+import styled from "styled-components";
 
 const StyledTitle = styled.h3`
   font-size: 1.5rem;
   text-align: center;
   justify-content: center;
-`
+`;
 
 const StyledContent = styled.span`
   font-size: 1rem;
   text-align: end;
-`
+`;
 
 const StyledAnchor = styled.a`
   color: #0070f3;
   text-decoration: none;
   font-size: 1rem;
-  `
-
+`;
 
 export default function Home() {
   return (
@@ -39,23 +38,36 @@ export default function Home() {
         <Header style={styles.header} />
         <h1 className={styles.title}>
           {/* Welcome to <span className={styles.span}>HuntZ</span> */}
-          <Image src="/huntzLogo.png" alt="huntzLogo" width={800} height={400} />
+          <Image
+            src="/huntzLogo.png"
+            alt="huntzLogo"
+            width={800}
+            height={400}
+          />
         </h1>
         <div className={styles.main}>
-         <StyledTitle>
-          GAME DOWNLOAD IS <strong>AVAILABLE</strong> !! 
+          <StyledTitle>
+            GAME DOWNLOAD IS <strong>AVAILABLE</strong> !!
           </StyledTitle>
         </div>
         <div className={styles.main}>
-        <Image src="/huntzMenu.png" alt="huntzMenuImage" width={877} height={500}/> 
-        <Image src="/huntz.png" alt="huntzImage" width={877} height={500}/>
+          <Image
+            src="/huntzMenu.png"
+            alt="huntzMenuImage"
+            width={877}
+            height={500}
+          />
+          <Image src="/huntz.png" alt="huntzImage" width={877} height={500} />
         </div>
         <div className={styles.main}>
-
-        <StyledContent>
-            Contact with me <StyledAnchor href="mailto:pabloriosrdn@gmail.com">via mail</StyledAnchor> if you want to be a tester!
+          <StyledContent>
+            Contact with me{" "}
+            <StyledAnchor href="mailto:pabloriosrdn@gmail.com">
+              via mail
+            </StyledAnchor>{" "}
+            if you want to be a tester!
           </StyledContent>
-          </div>
+        </div>
 
         {/* <div className={styles.main}>
           
@@ -63,25 +75,50 @@ export default function Home() {
           
         
         </div> */}
-        
 
         <Footer>
-          <a
-            href="https://github.com/gitradonz"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Github
-          </a>
-          <Image src="/react.svg" alt="reactImage" width="30px" height="30px" />
-          <Image
-            src="/vercel.svg"
-            alt="vercelImage"
-            width="50px"
-            height="50px"
-          />
+          <div className={styles.footerHome}>
+            <div>
+              <a
+                href="https://github.com/gitradonz"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  src="/github.svg"
+                  alt="gthubImage"
+                  width="30px"
+                  height="30px"
+                />
+              </a>
+            </div>
+            <div className={styles.spin}>
+              <Image
+                src="/react.svg"
+                alt="reactImage"
+                width="30px"
+                height="30px"
+              />
+            </div>
+            <div>
+              <Image
+                src="/vercel.svg"
+                alt="vercelImage"
+                width="50px"
+                height="50px"
+              />
+            </div>
+            <div>
+              <Image
+                src="/firebase.svg"
+                alt="firebaseImage"
+                width="30px"
+                height="30px"
+              />
+            </div>
+          </div>
         </Footer>
       </div>
     </>
-  )
+  );
 }
