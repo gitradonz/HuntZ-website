583a47ca7b2990e820b9e95ceed0d1b7ad02129e
Pablo Rios <pablo.rios@lobocom.es>
2022-05-18 11:59:22 +0200

    Added react-hook-form

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -2,16 +2,47 @@ import Footer from "../../components/footer";
 import Header from "../../components/header";
 import styles from "./Contact.module.css";
 import styled from "styled-components";
+import { useForm } from "react-hook-form";
 
 const StyledFooterText = styled.p`
   font-size: 0.6rem;
 `;
 
 export default function Contact() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   return (
     <div className={styles.container}>
       <Header />
-      <main className={styles.main}>CONTACT</main>
+      <main className={styles.main}>
+        <form
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+          })}
+        >
+          <div>
+            <input
+              {...register("email", { required: "Email required." })}
+              placeholder="Email.."
+            ></input>
+            <span>{errors.email?.message}</span>
+          </div>
+          <div>
+            <input
+              {...register("message", {
+                required: "Message required.",
+                minLength: { value: 10, message: "Minimum 10 characters." },
+              })}
+              placeholder="Message..."
+            ></input>
+            <span>{errors.message?.message}</span>
+          </div>
+          <button>ENVIAR</button>
+        </form>
+      </main>
       <Footer>
         <StyledFooterText>all right reserved</StyledFooterText>
       </Footer>
